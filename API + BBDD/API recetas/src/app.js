871ee4b905051fef8a1a885b19ff572d9cb6d603
Patricio Quintanilla <patricioquintanilladx@gmail.com>
2024-04-15 10:16:28 -0400

    CORS

--- API + BBDD/API recetas/src/app.js
+++ API + BBDD/API recetas/src/app.js
@@ -1,12 +1,16 @@
-import express from "express";
-import morgan from "morgan";
 // routes - rutas
 import LanguageRoutes from "./routes/language.routes"
+import express from "express";
+import morgan from "morgan";
 const app=express(); //ejecu express
+const cors = require('cors')
 // settings
 app.set("port", 4000);
 // middleware
 app.use(morgan("dev"));
+app.use(cors(
+    // {origin: ['http://localhost:4200', 'http://localhost:4000']}
+));
 app.use(express.json());
 
 // routes
