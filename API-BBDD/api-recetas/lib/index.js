f2c3c73457cd621d52f96c8a2ee887fabc23d7c8
Patricio Quintanilla <patricioquintanilladx@gmail.com>
2024-05-06 11:13:09 -0400

    actualiza api con babel

---
+++ API-BBDD/api-recetas/lib/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const app = require("./app");
+// import app from "./app"
+const main = () => {
+  app.listen(app.get("port"));
+  console.log(`Server on Port  ${app.get("port")}`);
+};
+main();
