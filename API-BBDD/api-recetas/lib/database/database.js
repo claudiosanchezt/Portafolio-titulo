2d5cfec2509acb758fa8a7ded6c62db502d27394
Patricio Quintanilla <patricioquintanilladx@gmail.com>
2024-05-06 11:13:09 -0400

    actualiza api con babel

---
+++ API-BBDD/api-recetas/lib/database/database.js
@@ -0,0 +1,53 @@
+"use strict";
+
+// import mysql from "promise-mysql";
+// import { claves } from "./../config";
+
+// const connection = mysql.createConnection({
+//     host: claves.host,
+//     database: claves.database,
+//     user: claves.user,
+//     password: claves.password
+// });
+
+// const getConnection = () => {
+//     return connection;
+// }
+
+// module.exports = {
+//     getConnection
+// }
+
+const {
+  Sequelize
+} = require('sequelize');
+const {
+  claves
+} = require('./../config');
+// Configuración de Sequelize
+const sequelize = new Sequelize(claves.database, claves.user, claves.password, {
+  host: claves.host,
+  dialect: 'mysql'
+});
+
+// Definición del modelo de conexión
+const Connection = sequelize.define('Connection');
+
+// Función para obtener la conexión
+const getConnection = async () => {
+  try {
+    // Autenticar la conexión
+    await sequelize.authenticate();
+    console.log('Conexión establecida correctamente.');
+    return sequelize;
+  } catch (error) {
+    console.error('No se pudo conectar a la base de datos:', error);
+    throw error;
+  }
+};
+
+// Exportar la función de conexión
+module.exports = {
+  getConnection,
+  Connection
+};
